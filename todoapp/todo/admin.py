03ea398c9ee10ed98f4eb495585c80dd9d984d8f
Harry <57110073+MRXTOOR@users.noreply.github.com>
2023-04-14 01:13:54 +0300

    100000IQ

--- todoapp/todo/admin.py
+++ todoapp/todo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Todo
 # Register your models here.
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'date', 'done') #какие поля моделей мы будем видеть в списке
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title')#по каким полям моделей мы можем искать
+    list_editable = ('done',)#что можем модифицировать
+    list_filter = ('done',)#как можем фильтровать список нашей модели
+
+
+admin.site.register(Todo, TodoAdmin)
