3b130b13ce10031cea1b175181776cd228f17e09
Harry <57110073+MRXTOOR@users.noreply.github.com>
2023-04-14 01:18:55 +0300

    bad

---
+++ todoapp/todo/api.py
@@ -0,0 +1,13 @@
+from .models import Todo
+from rest_framework import viewsets, permissions
+from .serializers import TodoSerializer
+
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = TodoSerializer
+
