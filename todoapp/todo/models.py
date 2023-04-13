a823b23e4104c74c49ec3d4351ea45e64e387c6d
Harry <57110073+MRXTOOR@users.noreply.github.com>
2023-04-14 02:08:44 +0300

    +react

--- todoapp/todo/models.py
+++ todoapp/todo/models.py
@@ -5,9 +5,9 @@ from django.db import models
 
 class Todo(models.Model):
     title = models.CharField(max_length=150)
-    description = models.CharField(max_length=400)
+    description = models.CharField(max_length=400, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    done = models.BooleanField(default=False)
+    done = models.BooleanField(default=False, null=False)
 
     def __str__(self):
         return self.title
